acace826a52bd4cb388ba759ff5ccd906e9737d3
Chris <62326766+apnaux@users.noreply.github.com>
2025-06-06 22:57:39 +0800

    chore: removed front-end related files

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthenticationController::class, 'register']);
 Route::post('/authenticate', [AuthenticationController::class, 'authenticate']);
-Route::post('/revoke', [AuthenticationController::class, 'revoke']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
+    Route::post('/revoke', [AuthenticationController::class, 'revoke']);
 });
